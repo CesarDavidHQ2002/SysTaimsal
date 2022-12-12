e94694b5c07ea586d2926edcda9480d776272995
cesardavidhq2002 <cesardavidHQ-2002@outlook.com>
2022-12-12 11:07:41 -0600

    Repositorio para Front end

---
+++ TaimsalSAdeSV/Controllers/Machinery.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaimsalSAdeSV.Controllers
+{
+    public class Machinery : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Portafolio()
+        {
+            return View();
+        }
+    }
+}
